4ac41929cec98fafa9bc143db5dd597f457c34d8
chawin <chawin15@gmail.com>
2019-12-09 09:24:35 +0800

    add personal theme stylings

---
+++ Personal-css-theme/theme.js
@@ -0,0 +1,34 @@
+const theme = {
+    fontFamily: 'Comic Sans MS',
+    heading: {
+        fontWeight: 700,
+        color: 'rgba(51, 51, 51, 0.7)',
+        letterSpacing: 2,
+    },
+    body: {
+        color: 'rgba(51, 51, 51, 0.7)',
+    },
+    icon: {
+        defaultColor: '#0084b4', // twitter dark blue
+        mailIcon: '#ff4c4c',
+        linkedinIcon: "#0072b1",
+        githubIcon: "#6e5494",
+        youtubeIcon: "#c4302b",
+        facebookIcon: "#3b5998"
+    },
+    centering: {
+        textAlign: "center",
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        webkitTransform: "translate(-50%, -50%)",
+        transform: "translate(-50%, -50%)"
+    },
+    link: {
+        color: "#0084b4",
+        fontFamily: "Comic Sans MS"
+    },
+    navBars: {
+        backgroundColor: "#ffa500"
+    }
+};
